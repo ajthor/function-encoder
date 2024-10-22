fc3b8957c0c7c5fd45dd9db7e8ff9de022568cc1
Adam Thorpe <adam.thorpe.g@gmail.com>
2024-10-22 16:23:27 -0500

    Update losses.py

--- function_encoder_torch/losses.py
+++ function_encoder_torch/losses.py
@@ -1,15 +1,15 @@
 import torch
 
 
-def basis_normalization_loss(G):
+def basis_normalization_loss(K):
     # Penalize the diagonal of the gram matrix being far from one.
-    return ((torch.diagonal(G, dim1=-2, dim2=-1)) ** 2).mean()
+    return ((torch.diagonal(K, dim1=-2, dim2=-1)) ** 2).mean()
 
 
-def basis_orthonormality_loss(G, device):
+def basis_orthonormality_loss(K, device):
     # Penalize the gram matrix being far from the identity.
-    identity_matrix = torch.eye(G.shape[-1], device=device)
-    gram_matrix_penalty = (G - identity_matrix).norm(dim=(1, 2)).mean()
+    identity_matrix = torch.eye(K.shape[-1], device=device)
+    gram_matrix_penalty = (K - identity_matrix).norm(dim=(1, 2)).mean()
     return gram_matrix_penalty
 
 
