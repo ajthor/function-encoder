93afed2f1389388eb2763009a6d02d2628ae4a6d
Adam Thorpe <adam.thorpe.g@gmail.com>
2025-01-27 14:48:10 -0600

    Add multi-headed MLP.

--- function_encoder/model/mlp.py
+++ function_encoder/model/mlp.py
@@ -30,3 +30,39 @@ class MLP(torch.nn.Module):
         x = self.layers[-1](x)
 
         return x
+
+
+class MultiHeadedMLP(torch.nn.Module):
+    def __init__(
+        self,
+        layer_sizes: List[int],
+        num_heads: int,
+        activation: Callable = torch.nn.Tanh(),
+        bias: bool = True,
+    ):
+        super(MultiHeadedMLP, self).__init__()
+
+        self.layer_sizes = layer_sizes
+        self.activation = activation
+
+        self.layers = torch.nn.ModuleList()
+
+        for i in range(len(layer_sizes) - 2):
+            self.layers.append(
+                torch.nn.Linear(layer_sizes[i], layer_sizes[i + 1], bias=bias),
+            )
+
+        self.layers.append(
+            torch.nn.Linear(layer_sizes[-2], layer_sizes[-1] * num_heads, bias=bias),
+        )
+
+    def forward(self, x):
+        for layer in self.layers[:-1]:
+            x = layer(x)
+            x = self.activation(x)
+
+        x = self.layers[-1](x)
+
+        x = x.view(*x.shape[:-1], self.layer_sizes[-1], -1)
+
+        return x
